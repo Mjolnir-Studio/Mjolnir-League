4a1f8f7cf4d2019b33d6809c4bc01c0411aef842
yomisana <sammywhy3236@gmail.com>
2022-07-29 12:54:09 +0800

    web 1.0

---
+++ src/resource/plugin/index.js
@@ -0,0 +1,16 @@
+function homepage() {
+document.getElementById("page_container").innerHTML='<iframe src="splash.html"></iframe>';
+}
+function summonerpage() {
+document.getElementById("page_container").innerHTML='<iframe src="summoner.html" ></iframe>';
+}
+function battlepage() {
+document.getElementById("page_container").innerHTML='<iframe src="battle.html" ></iframe>';
+}
+function chatpage() {
+document.getElementById("page_container").innerHTML='<iframe src="chat.html" ></iframe>';
+}
+
+function settingspage() {
+document.getElementById("page_container").innerHTML='<iframe src="settings.html" ></iframe>';
+}
